cb758e47a0ec99245fa3470180fe3ec37f534271
Jefferson Barroso Freitas <jeferssonbarroso@hotmail.com>
2023-11-28 14:25:45 -0300

    Final Update

--- src/db/DB.java
+++ src/db/DB.java
@@ -4,12 +4,13 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
 
 public class DB {
-	//Pra conectar com o banco de dados
+
 	private static Connection conn = null;
 	
 	public static Connection getConnection() {
@@ -17,55 +18,53 @@ public class DB {
 			try {
 				Properties props = loadProperties();
 				String url = props.getProperty("dburl");
-				conn = DriverManager.getConnection(url,props);
+				conn = DriverManager.getConnection(url, props);
 			}
-			catch (SQLException e){
+			catch (SQLException e) {
 				throw new DbException(e.getMessage());
 			}
-	}
+		}
 		return conn;
 	}
+	
 	public static void closeConnection() {
-		if(conn != null) {
+		if (conn != null) {
 			try {
 				conn.close();
-			}
-			catch(SQLException e) {
+			} catch (SQLException e) {
 				throw new DbException(e.getMessage());
 			}
 		}
 	}
 	
-	//Pra carregar infos do db.propeties
 	private static Properties loadProperties() {
-		try (FileInputStream fs = new FileInputStream("db.propeties")){
-			Properties props = new Properties(); 
+		try (FileInputStream fs = new FileInputStream("db.properties")) {
+			Properties props = new Properties();
 			props.load(fs);
 			return props;
-		} catch(IOException e) {
+		}
+		catch (IOException e) {
 			throw new DbException(e.getMessage());
 		}
-		
 	}
 	
 	public static void closeStatement(Statement st) {
-		if(st != null) {
+		if (st != null) {
 			try {
 				st.close();
 			} catch (SQLException e) {
-				throw new DbExcepion(e.getMessage());
-			}
-		}
-	}
-	
-	public static void closeResultSet(ResultSet rs) {
-		if(rs != null) {
-			try {
-				rs.close();
-			} catch (SQLException e) {
-				throw new DbExcepion(e.getMessage());
+				throw new DbException(e.getMessage());
 			}
 		}
 	}
 
+	public static void closeResultSet(ResultSet rs) {
+		if (rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
 }
--- src/model/dao/DepartmentDAO.java
+++ src/model/dao/DepartmentDAO.java
@@ -4,10 +4,10 @@ import java.util.List;
 
 import model.entities.Department;
 
-public interface DepartmentDAO {
-	
+public interface DepartmentDao {
+
 	void insert(Department obj);
-	void update(Dapartment obj); 
+	void update(Department obj);
 	void deleteById(Integer id);
 	Department findById(Integer id);
 	List<Department> findAll();
